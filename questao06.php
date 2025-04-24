aa74a5f8fa17593caa7c4ba0f0cdfb6e24ccb324
GitCida <mariaaparecidaily2@gmail.com>
2025-04-24 19:19:28 -0300

    Enviando questões

--- questao06.php
+++ questao06.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questão 6</title>
+    <title>Questão 06</title>
 </head>
 <body>
     <?php 
